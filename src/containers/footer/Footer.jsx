29d19b0ae3c8c319ecc838dc3d5e3b81a9175639
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-05-29 15:30:09 +0530

    first commit

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        {/* <p>Mangalore,India <br /></p> */}
+      </div>
+      {/* <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div> */}
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <a href='https://github.com/SouravSreelan'><p>Sourav Sreelan</p></a>
+        <p>souravsreelan@gmail.cm</p>
+       
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2024 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
