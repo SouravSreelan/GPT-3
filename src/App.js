8f100550a4f7afa88b2f8559de26a879718b5e1d
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-07-16 10:30:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { CTA, Brand, Navbar } from './components'
-import './App.css'
+import './App.css' 
 
 const App = () => {
   return (
