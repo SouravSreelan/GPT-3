bd7a2047711128e281170862948325c256d6d2e9
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-08-02 09:53:10 +0530

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './brand.css'
 import { google,slack,atlassian,dropbox,shopify } from './import'
 
-
 const Brand = () => {
   return (
     <div className='gpt3__brand section__padding'>
