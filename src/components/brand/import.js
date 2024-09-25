3ff7ccb58d6613454c7099bea04c056631de536a
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-09-25 09:51:22 +0530

    Update import.js

--- src/components/brand/import.js
+++ src/components/brand/import.js
@@ -2,7 +2,7 @@ import google from '../../assets/google.png'
 import slack from '../../assets/slack.png'
 import atlassian from '../../assets/atlassian.png'
 import dropbox from '../../assets/dropbox.png'
-import shopify from '../../assets/shopify.png'
+import shopify from '../../assets/shopify.png' 
 
 export {
     google,
