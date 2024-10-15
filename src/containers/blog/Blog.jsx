a0fe375baaeb5aabca936fcd95c9cdc95e78e24c
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-10-15 09:42:06 +0530

    Update Blog.jsx

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Article from '../../components/article/Article';
 import { blog01, blog02, blog03, blog04, blog05 } from './import';
-import './blog.css';
+import './blog.css'; 
 
 const Blog = () => (
   <div className="gpt3__blog section__padding" id="blog">
